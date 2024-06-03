53c505745b4bcd7c86e5d179d4dfbd4b90557cdc
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-03 14:41:57 -0400

    Add functions to calculate total payroll owed to employees

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+const createEmployeeRecord = (employee) => {
+  return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+};
+
+const createEmployeeRecords = (employees) => {
+  return employees.map(createEmployeeRecord)
+}
+
+const createTimeInEvent = (employeeRecord, dateStamp) => {
+  const [date, hour] = dateStamp.split(' ')
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date
+  })
+  return employeeRecord
+}
+
+const createTimeOutEvent = (employeeRecord, dateStamp) => {
+  const [date, hour] = dateStamp.split(' ')
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date
+  })
+  return employeeRecord
+}
+
+const hoursWorkedOnDate = (employeeRecord, dateStamp) => {
+ const timeInSearchDate = employeeRecord.timeInEvents.find(event => event.date === dateStamp).hour
+ const timeOutSearchDate = employeeRecord.timeOutEvents.find(event => event.date === dateStamp).hour
+ return (timeOutSearchDate - timeInSearchDate) / 100
+}
+
+const wagesEarnedOnDate = (employeeRecord, dateStamp) => {
+  return hoursWorkedOnDate(employeeRecord, dateStamp) * employeeRecord.payPerHour
+}
+
+const allWagesFor = (employeeRecord) => {
+  const datesWorked = employeeRecord.timeInEvents.map(event => event.date)
+  return datesWorked.reduce((total, date) => {
+    return total + wagesEarnedOnDate(employeeRecord, date)
+  }, 0)
+}
+
+const calculatePayroll = (employeeRecords) => {
+ return employeeRecords.reduce((total, employeeRecord) => {
+    return total + allWagesFor(employeeRecord)
+  }, 0)
+}
